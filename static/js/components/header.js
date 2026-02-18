17039d409d71017bfba97e51bf826ac4d276deb5
= <=>
2026-02-18 11:04:49 +0530

    Updated header and colors.

--- static/js/components/header.js
+++ static/js/components/header.js
@@ -1,5 +1,7 @@
 // ? GETTING DOC ELEMENTS
 const themeControl = document.getElementById("theme-control");
+const menuBtns = document.querySelectorAll(".menu-btn");
+const navbar = document.querySelector(".nav");
 
 // * FUNCTION TO SAVE THEME PREFERENCE
 function saveThemePreference(theme) {
@@ -23,6 +25,30 @@ function toggleTheme() {
     saveThemePreference(document.body.classList.contains('dark') ? 'dark' : 'light');
 }
 
+// * FUNCTION TO TOGGLE NAVBAR
+function toggleNav() {
+    if (navbar.classList.contains('show')) {
+        navbar.classList.remove('show');
+
+        setTimeout(() => {
+            navbar.style.display = 'none';
+        }, 500);
+    } else {
+        navbar.style.display = 'flex';
+
+        setTimeout(() => {
+            navbar.classList.add('show');
+        }, 100);
+    }
+}
+
 // & EVENT LISTENER FOR THEME TOGGLE
 themeControl.addEventListener('click', toggleTheme);
 loadThemePreference();
+
+// & EVENT LISTENER FOR NAVBAR TOGGLE
+menuBtns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        toggleNav();
+    });
+});
